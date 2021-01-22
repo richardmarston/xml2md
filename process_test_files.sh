949e3e8ba371369f7ba50ba0e8298f6007a3f19d
Richard Marston <rmarston@eonerc.rwth-aachen.de>
2021-01-22 14:30:46 +0100

    Create folder link per directory
    Signed-off-by: Richard Marston <rmarston@eonerc.rwth-aachen.de>

--- process_test_files.sh
+++ process_test_files.sh
@@ -9,26 +9,21 @@ fi
 
 mkdir -p ${OUTPUT_DIR}
 
-cat > ${OUTPUT_DIR}/_index.md <<EOF
----
-title: "Bridge"
-linkTitle: "Bridge"
-weight: 5
----
-EOF
-
+echo "PROCESSING GRUPOETRA"
 # process all *.xml files from grupoetra and create index.md
 find grupoetra/* -type f -name '*.xml' -exec sh -c '
-  file="$0"
+  set -x
+  FILE="$0"
   OUTPUT_DIR="$1"
-  dirnameprefix=$(dirname "${file}")
-  DIRNAME=${dirnameprefix#"grupoetra/"}
+  DIRNAMEPREFIX=$(dirname "${FILE}")
+  DIRNAME=${DIRNAMEPREFIX#"grupoetra/"}
   mkdir -p ${OUTPUT_DIR}/${DIRNAME}
-  output_file_name="${OUTPUT_DIR}/${DIRNAME}/index.md"
-  echo "$output_file_name"
-  python3 xml2md.py "$file" > "$output_file_name"
+  OUTPUT_FILE_NAME="${OUTPUT_DIR}/${DIRNAME}/index.md"
+  echo "$OUTPUT_FILE_NAME"
+  python3 xml2md.py "$FILE" > "$OUTPUT_FILE_NAME"
 ' {} ${OUTPUT_DIR} ';'
 
+echo "PROCESSING XML2MD-INPUT"
 # process all *.xml files from xml2md-input and create index.md
 find xml2md-input/* -type f -name '*.xml' -exec sh -c '
   file="$0"
@@ -50,4 +45,27 @@ find xml2md-input/* -type f -name '*.png' -exec sh -c '
   DIRNAME=${dirnameprefix#"xml2md-input/"}
   output_file_name="${OUTPUT_DIR}/${DIRNAME}/${basename}"
   cp $file $output_file_name
+' {} ${OUTPUT_DIR} ';'
+
+echo "CREATING DIRECTORIES"
+find "${OUTPUT_DIR}" -type d -exec sh -c '
+  DIRPATH=$0
+  echo $DIRPATH
+  DIRNAME=$(basename $DIRPATH)
+  echo $DIRNAME
+  if [ ! -f "${DIRPATH}/index.md" ]
+  then
+    echo DNE: "${DIRPATH}/index.md"
+    if [ ! -f "${DIRPATH}/_index.md" ]
+    then
+      echo DNE: "${DIRPATH}/_index.md"
+      cat > ${DIRPATH}/_index.md <<EOF
+---
+title: "$DIRNAME"
+linkTitle: "$DIRNAME"
+weight: 5
+---
+EOF
+    fi
+  fi
 ' {} ${OUTPUT_DIR} ';'
